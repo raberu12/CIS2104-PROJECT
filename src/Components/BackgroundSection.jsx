ae5a83bb1770b067edef013a2618a64d35e7e0f2
Matt <11400071@usc.edu.ph>
2023-10-10 19:49:50 +0800

    cleaned up css and added more content

--- src/Components/BackgroundSection.jsx
+++ src/Components/BackgroundSection.jsx
@@ -5,6 +5,7 @@ const BackgroundSection = () =>{
         <div className="background-section">
             <h1>IT'S ALL ABOUT WHAT YOU CAN ACHIEVE</h1>
             <h3>Take Control, transform your soul</h3>
+            <button>GET STARTED</button>
         </div>
     );
 };
